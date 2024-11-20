b5ae93ec2ed8d1e98ede5ca468881e83f112bdf0
Trevor Apple <trevorapple@Trevors-MacBook-Air-2.local>
2024-11-19 20:07:03 -0800

    added more working instructions to list41.asm

--- test_asm/list41.asm
+++ test_asm/list41.asm
@@ -21,3 +21,13 @@ add bx, [bp]
 add si, 2
 add bp, 2
 add cx, 8
+add bx, [bp + 0]
+add cx, [bx + 2]
+add bh, [bp + si + 4]
+add di, [bp + di + 6]
+add [bx+si], bx
+add [bp], bx
+add [bp + 0], bx
+add [bx + 2], cx
+add [bp + si + 4], bh
+add [bp + di + 6], di
